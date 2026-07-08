f69cdedf9a3beeb4be1a673230e8a6975575fdec
toobaj522-debug <toobaj522@gmail.com>
2026-07-08 17:45:12 +0500

    Add files via upload
    Start your journey of c++ with me, contact if u have any question.

---
+++ Day 3.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+main()
+{
+	int num1= 5, num2=7;
+	// You can store more than one value if data type is same, using comma(,)
+	
+	cout<< num1 + num2 <<endl;
+	// You can use any arithmetic operation
+}
