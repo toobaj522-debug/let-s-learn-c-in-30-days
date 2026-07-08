e1638d2dfe53d7b2f1f62d254d62b7ae56ecb36b
toobaj522-debug <toobaj522@gmail.com>
2026-07-08 17:44:52 +0500

    Add files via upload
    Start your journey of c++ with me, contact if u have any question.

---
+++ Day 2.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+main()
+{
+	string name;
+	// string is data type which is used to store alphabet words or lines
+	
+	cout<<"Enter your name: "<<endl;
+	cin>>name;
+	//cin is used for give name by user
+	
+	cout<<"Your name is "<< name<<endl;
+}
